9bf63a02301b9eef42cfbd4e3a2bd8b9b2163467
ccine <56122695+ccine@users.noreply.github.com>
2024-08-14 16:46:14 +0200

    Fixed db file path

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-const db = new sqlite3.Database("chamberData.db");
+const db = new sqlite3.Database("../chamberData.db");
 
 // Creazione della tabella se non esiste
 db.serialize(() => {
